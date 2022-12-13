d8ba499a4cf4888fc7c32b822cc1f10c42955148
Angelo Casciani <angelocasciani@live.it>
2022-12-13 11:45:56 +0100

    First implementation of Eager RB and Probabilistic Broadcast

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ from node import Node
 from threading import Thread
 import time
 
-n_nodes = 7
-channel_bandwidth = 10
+### PRINT TITLE OF THE FRAMEWORK
+### Take inputs for filing variables
+
+n_nodes = 4
+arrival_rate = 0.2   # lambda
+channel_bandwidth = 10   # mu
+broadcast_type = "eager_prob"
 start_time = time.time()
+may_crash = False
+# If prob brod
+fan_out = 2
 
 def launch_node(id_node):
-    Node(id_node, n_nodes, "lazy_rb", start_time)
+    Node(id_node, n_nodes, broadcast_type, arrival_rate, start_time, may_crash, fan_out)
 
 def launch_channel(node1, node2):
     Channel(node1, node2, channel_bandwidth)
    
 
-for n in range(4):
+for n in range(n_nodes):
     Thread(target = launch_node, args = [n], daemon = True).start()
 
-for m in range(4):
-    for n in range(m,4):
+for m in range(n_nodes):
+    for n in range(m, n_nodes):
         Thread(target = launch_channel, args = [m,n], daemon = True).start()
 
 input('Press ENTER to quit\n')
--- node.py
+++ node.py
@@ -6,29 +6,39 @@ import udp_support as udp
 import numpy as np
 
 class Node:
-    def __init__(self, id, n_nodes, broadcast_type, start_time):
+    def __init__(self, id, n_nodes, broadcast_type, arrival_rate, start_time, may_crash, prob_k):
         self.port = 9000 + id
-        self.id = id
-        self.type = broadcast_type
+        self.id = int(id)
         self.start_time = start_time
-        # In case of eager_rb
-        self.delivered = deque(maxlen = 200)
-        self.alive = True
+        self.arrival_rate = arrival_rate
+        # Lazy RB
+        self.correct = set(range(n_nodes))
+        self.message_from = None
+        # Eager RB and Eager PB
+        self.delivered = None
+        self.fan_out = None
+
+        self.type = broadcast_type
         self.deliver = None
-        if broadcast_type == "beb":
-            self.deliver = self.beb_deliver
-        elif broadcast_type == "lazy_rb":
+        if broadcast_type == "lazy_rb":
             self.deliver = self.lazy_rb_deliver
+            self.message_from = deque(maxlen = 200)
         elif broadcast_type == "eager_rb":
-            self.deliver = self.lazy_rb_deliver
+            self.deliver = self.eager_rb_deliver
+            self.delivered = deque(maxlen = 200)
         elif broadcast_type == "eager_prob":
             self.deliver = self.eager_probabilistic_deliver
-        self.correct = set(range(n_nodes))
-        self.message_from = deque(maxlen = 200)
+            self.delivered = deque(maxlen = 200)
+            self.fan_out = prob_k
+        
         self.receive_thread = Thread(target = udp.start_udp_server, daemon = True, args = [self])
         self.receive_thread.start()
-        self.crash_thread = Thread(target = self.crash, daemon = True)
-        self.crash_thread.start()
+        
+        self.alive = True
+        self.may_crash = may_crash
+        if self.may_crash:
+            self.crash_thread = Thread(target = self.crash, daemon = True)
+            self.crash_thread.start()
         self.start_node()
 
     def broadcast(self, message):
@@ -41,19 +51,49 @@ class Node:
             print(f"[{time.time() - self.start_time}][{tag.upper()}] Process {self.id} broadcasts message {content} with sender {sender_id}")
         
         for node in self.correct:
-            list_nodes = list()
-            list_nodes.append(self.id)
-            list_nodes.append(node)
-            list_nodes.sort()
             if self.id <= node:
                 channel_port = 9050 + int(f"{self.id}" + f"{node}")
             else:
                 channel_port = 9050 + int(f"{node}" + f"{self.id}")
             
             udp.udp_send(channel_port, message)
+    
+    def pick_targets(self):
+        targets = deque(maxlen = self.fan_out)
+        candidates = list(self.correct)
+        candidates.remove(self.id)
+        while len(targets) != self.fan_out:
+            candidate = random.choice(candidates)
+            if candidate not in targets:
+                targets.append(candidate)
+        return targets
 
-    def beb_deliver(self, message):
-        print(f"[BEB_DELIVERY] Process{self.id} delivers from process{message}")
+    def gossip(self, message):
+        targets = self.pick_targets()
+        for n in targets:
+            if self.id <= n:
+                channel_port = 9050 + int(f"{self.id}" + f"{n}")
+            else:
+                channel_port = 9050 + int(f"{n}" + f"{self.id}")
+            udp.udp_send(channel_port, message)
+
+    def prob_broadcast(self, message):
+        message_list = message.strip().split("_")
+        sender_id = int(message_list[0])
+        tag = message_list[1]
+        content = message_list[2]
+        time_sent = message_list[3]
+        current_round = int(message_list[4])
+
+        if tag == "broadcast":
+            content = message_list[2]
+            print(f"[{time.time() - self.start_time}][{tag.upper()}] Process {self.id} broadcasts message {content} with sender {sender_id}")
+            print(message)
+
+        self.delivered.append(message)
+        print(f"[{time.time() - self.start_time}][PROB_DELIVERY] Process {self.id} delivers message {content} with sender {sender_id}")  # Deliver to the application    
+        self.eager_probabilistic_deliver(message)
+        self.gossip(message)
 
     def lazy_rb_deliver(self, message):
         message_list = message.strip().split("_")
@@ -71,22 +111,47 @@ class Node:
             print(f"[{time.time() - self.start_time}][NO DELIVERY] Process {self.id} already delivered message {content} with sender {sender_id}")
 
     def eager_rb_deliver(self, message):
-        pass
+        message_list = message.strip().split("_")
+        sender_id = int(message_list[0])
+        content = message_list[2]
+        time_sent = message_list[3]
+
+        if message not in self.delivered:
+            self.delivered.append(message)
+            print(f"[{time.time() - self.start_time}][ERB_DELIVERY] Process {self.id} delivers message {content} with sender {sender_id}")  # Deliver to the application    
+            self.broadcast(message)
+        else:
+            print(f"[{time.time() - self.start_time}][NO DELIVERY] Process {self.id} already delivered message {content} with sender {sender_id}")
 
     def eager_probabilistic_deliver(self, message):
-        pass
-    
+        print(message)
+        message_list = message.strip().split("_")
+        sender_id = int(message_list[0])
+        content = message_list[2]
+        time_sent = message_list[3]
+        current_round = int(message_list[4])
+        
+        if message not in self.delivered:
+            self.delivered.append(message)
+            print(f"[{time.time() - self.start_time}][PROB_DELIVERY] Process {self.id} delivers message {content} with sender {sender_id}")  # Deliver to the application    
+        
+        if current_round > 1:
+            current_round -= 1
+            message = message[0:-3] + f"{current_round}\n"
+            self.gossip(message)
+
     # Method corresponding to the Crash event in our algorithms.
     def receive_crash(self, id):
-        self.correct.remove(int(id))
-        print(f"[{time.time() - self.start_time}][CRASH] Process {self.id} detects crash of process {id}")
-        for message in self.message_from:
-            message_list = message.strip().split("_")
-            sender_id = message_list[0]
-            tag = message_list[1]
-            content = message_list[2]
-            if sender_id == id and tag != "crash":
-                self.broadcast(message)
+        if self.type == "lazy_rb":
+            self.correct.remove(int(id))
+            print(f"[{time.time() - self.start_time}][CRASH] Process {self.id} detects crash of process {id}")
+            for message in self.message_from:
+                message_list = message.strip().split("_")
+                sender_id = message_list[0]
+                tag = message_list[1]
+                content = message_list[2]
+                if sender_id == id and tag != "crash":
+                    self.broadcast(message)
     
     # Method to simulate a the crash of a process. The perfect failure detector is not directly implemented but simulated by the sending
     # of a crash message from the process that is crashing.
@@ -100,7 +165,7 @@ class Node:
             self.broadcast(f"{self.id}_crash\n")
     
     def start_node(self):
-        scale_beta = 5
+        scale_beta = 1/self.arrival_rate
         time_instants = np.random.exponential(scale_beta, 20)
 
         for t in time_instants:
@@ -109,6 +174,13 @@ class Node:
                 return
             # Message format: senderID_tag_content_time\n
             message = f"{self.id}_broadcast_{random.randint(1, 100)}_{time.time()}\n"
-            self.broadcast(message)
+
+            if self.type == "eager_prob":
+                rounds = 3
+                # Insert in the message the information about the number of rounds to perform
+                message = f"{self.id}_broadcast_{random.randint(1, 100)}_{time.time()}_{rounds}\n"
+                self.prob_broadcast(message)
+            else:
+                self.broadcast(message)
 
 
